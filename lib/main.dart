be01b12d18e13a8ad02e2023967dd38849c044be
shokiogawa <syoki2578@outlook.jp>
2020-10-01 17:38:33 +0900

    add page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:practicehome/view/screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
---
+++ lib/view/screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyhomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
